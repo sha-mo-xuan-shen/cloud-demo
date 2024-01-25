560bed3c45d5b815b7d76b4bd193da472d1ca070
sha-mo-xuan-shen <2052039701@qq.com>
2024-01-25 17:35:34 +0800

    阿米诺斯

--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,18 +1,29 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private PatternProperties properties;
+//    @Value("${pattern.dateformat}")
+//    String dateformat;
 
     /**
      * 路径： /user/110
@@ -23,5 +34,13 @@ public class UserController {
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
+    }
+//    @GetMapping("/otto")
+//    public String getDateformat(){
+//        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+//    }
+    @GetMapping("/prop")
+    public PatternProperties getEnvSharedValue(){
+        return properties;
     }
 }
